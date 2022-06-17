f9c7c7c0b5ee1205e408ff90ec40b5364696acc0
Michael M <michaelbelay818@gmail.com>
2022-06-17 11:55:02 -0400

    Completed the gameRound method

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 
-let playerSelection = "rock";
-let computerSelection = computerPlay();
+
 // This function will randomly return Rock, Paper, or Scissors
 function computerPlay() {
     let randomNum = Math.floor(Math.random() * 3);
@@ -19,9 +18,40 @@ function computerPlay() {
 
 // This function will return the winner of each round
 function gameRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
 
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You Lose! Paper beats Rock";
+        } else if (computerSelection === "scissors") {
+            return "You Win! Rock beats Scissors";
+        } else {
+            return "Draw";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You Lose! Scissors beats Paper";
+        } else if (computerSelection === "rock") {
+            return "You Win! Paper beats Rock ";
+        } else {
+            return "Draw";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "You Lose! Rock beats Scissors";
+        } else if (computerSelection === "paper") {
+            return "You Win! Scissors beats Paper";
+        } else {
+            return "Draw";
+        }
+    } else {
+        return "Invalid Input";
+    }
 }
 
+let playerSelection = "rock";
+let computerSelection = computerPlay();
+console.log(gameRound(playerSelection, computerSelection));
 // This is in charge of the game
 function game() {
 
